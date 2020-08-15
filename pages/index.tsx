fc157bed3e25867a69c5e98c67022d3159179534
Yuya Oiwa <yuya.oiwa@me.com>
2020-08-15 18:03:09 +0900

    add doc

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,27 @@ import styles from "../styles/Home.module.css";
 
 import { getGithubPreviewProps, parseJson } from "next-tinacms-github";
 import { GetStaticProps } from "next";
+import { usePlugin } from "tinacms";
+import {
+  useGithubJsonForm,
+  useGithubToolbarPlugins,
+} from "react-tinacms-github";
 
 export default function Home({ file }) {
-  const data = file.data;
+  const formOptions = {
+    label: "Home Page",
+    fields: [{ name: "title", component: "text" }],
+  };
+
+  const [data, form] = useGithubJsonForm(file, formOptions);
+  usePlugin(form);
+
+  useGithubToolbarPlugins();
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Create Next App!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
